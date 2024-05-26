fbc846bf018ecb483dcf90ebe59482544da608df
Rayyaan-1 <rabdul-hamid003@st.ug.edu.gh>
2024-05-26 17:04:43 +0000

    cmt1

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text} >My name is <Text style={styles.bold}>Rayyaan</Text>
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000080',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
