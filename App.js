d15ffe3b5fcf42ea3aabdce29d38a463adf03ee8
Rayyaan-1 <rabdul-hamid003@st.ug.edu.gh>
2024-05-27 02:30:37 +0000

    cmmt

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000080',
+    backgroundColor: '#ADD8E6',
     alignItems: 'center',
     justifyContent: 'center',
   },
